51bbf0c7ecfc00c153166dec3df1c7a10932de68
MaryHooker <mc020791@yahoo.com>
2020-04-24 21:29:40 -0500

    client displaying in browser

---
+++ todos-app/client/src/components/AppContainer.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>To Do's</div>
+         );
+    }
+}
+ 
+export default AppContainer;
