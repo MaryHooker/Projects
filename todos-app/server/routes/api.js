86829803e8b29340a15bb03f9b92c7dae36de6e7
MaryHooker <mc020791@yahoo.com>
2020-04-24 21:20:52 -0500

    Bill routes connected to database

--- todos-app/server/routes/api.js
+++ todos-app/server/routes/api.js
@@ -122,31 +122,46 @@ router.get('/personal', (req,res) => {
 //Create request
 router.post('/bill',(req,res) => {
     console.log(`Create bill request!`)
-    res.send(`Create bill request!`);
+    // res.send(`Create bill request!`);
+    BillsCollection.create(req.body, (errors,results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Read request by BILL
 router.get('/bill/:bill', (req,res) => {
     console.log(`Read bill request by bill!`);
-    res.send(`Read bill request by bill!`);
+    // res.send(`Read bill request by bill!`);
+    BillsCollection.findOne({bill:req.params.bill}, (errors,results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Update request by BILL
 router.put('/bill/:bill', (req,res) => {
     console.log(`Update bill request by bill!`);
-    res.send(`Update bill request by bill!`);
+    // res.send(`Update bill request by bill!`);
+    BillsCollection.findOneAndUpdate({bill:req.params.bill}, req.body, {new:true}, (errors,results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Delete request by BILL
 router.delete('/bill/:bill', (req,res) => {
     console.log(`Delete bill request by bill!`);
-    res.send(`Delete bill request by bill!`);
+    // res.send(`Delete bill request by bill!`);
+    BillsCollection.findOneAndDelete({bill:req.params.bill}, (errors,results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 //Read all request
 router.get('/bill', (req,res) => {
     console.log(`Read all bill requests!`);
-    res.send(`Read all bill requests!`);
+    // res.send(`Read all bill requests!`);
+    BillsCollection.find({}, (errors,results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 })
 
 // //sanity
