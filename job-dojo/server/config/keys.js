be45c2006b7f4607bb418e4e1846e965cd06abe3
MaryHooker <mc020791@yahoo.com>
2020-09-24 20:35:16 -0500

    job crud routes set up and connected to mongoDB/PM test cases updated

--- job-dojo/server/config/keys.js
+++ job-dojo/server/config/keys.js
@@ -1,7 +1,7 @@
 //File to hold connection string to MongoDB/ Secret Key for authorization/ and the port number
 
 module.exports = {
-    mongoURI:'mongodb+srv://Student:C0d3Cr3w@cluster0-p8fhv.mongodb.net/passion_project?retryWrites=true&w=majority',
+    mongoURI:'mongodb+srv://Student:C0d3Cr3w@cluster0-p8fhv.mongodb.net/cs_database?retryWrites=true&w=majority',
     secretKey:"",
     port: 1991
 }
--- job-dojo/server/routes/api.js
+++ job-dojo/server/routes/api.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //reference authorizations
 
 //import schemas/models
-// const JobCollection = require('../model/JobSchema');
+const JobCollection = require('../model/JobSchema');
 
 /////////////////////////////////////////////////////////////////////
 //    Jobs
@@ -17,29 +17,51 @@ const router = express.Router();
 router.post('/jobs',(req,res) => {
     //sanity
     console.log('Job Created');
-    res.send('Job CREATED!');
+    // res.send('Job CREATED!');
+    JobCollection.create(req.body, (errors,results) => {
+        errors ? res.send(errors): res.send(results);
+    })
 });
 
 //Read a job by title
-router.get('/jobs/:title',(req,res) => {
+router.get('/jobs/:company',(req,res) => {
     //sanity
     console.log('Viewing specific job!');
-    res.send('viewing one job');
+    // res.send('viewing one job');
+    JobCollection.findOne({company:req.params.company}, (errors,results) => {
+        errors ? res.send(errors): res.send(results);
+    })
 });
 
 //Update a job by title
-router.put('/jobs/:title',(req,res) => {
+router.put('/jobs/:company',(req,res) => {
     //sanity
     console.log('Updating specific job!');
-    res.send('Updating job');
+    // res.send('Updating job');
+    JobCollection.findOneAndUpdate({company:req.params.company}, req.body, (errors,results) => {
+        errors ? res.send(errors): res.send(results);
+    })
 });
 
 
 //Delete a job by title
-router.delete('/jobs/:title',(req,res) => {
+router.delete('/jobs/:company',(req,res) => {
     //sanity
     console.log('Deleting specific job!');
-    res.send('Deleting job');
+    // res.send('Deleting job');
+    JobCollection.findOneAndDelete({company:req.params.company}, (errors,results) => {
+        errors ? res.send(errors): res.send(results);
+    })
+});
+
+//Viewing all jobs
+router.get('/jobs',(req,res) => {
+    //sanity
+    console.log('Viewing all jobs!');
+    // res.send('viewing one job');
+    JobCollection.find({}, (errors,results) => {
+        errors ? res.send(errors): res.send(results);
+    })
 });
 
 
