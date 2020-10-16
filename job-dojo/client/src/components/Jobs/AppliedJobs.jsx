f9446b956c88a609724eb745eedcdd6862b68b07
MaryHooker <mc020791@yahoo.com>
2020-10-16 17:30:28 -0500

    console logging applied jobs from database

--- job-dojo/client/src/components/Jobs/AppliedJobs.jsx
+++ job-dojo/client/src/components/Jobs/AppliedJobs.jsx
@@ -3,8 +3,25 @@ import React,{Component} from 'react';
 class AppliedJobs extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            appliedJobs:[],
+         }
     }
+
+    componentDidMount(){
+        this.loadAllJobs();
+    }
+
+    loadAllJobs = async() => {
+        let response = await fetch(`/api/jobs`,{
+            method:"GET"
+        })
+        let json = await response.json();
+        //sanity
+        console.table(json);
+    }
+
+
     render() { 
         return ( <div>
             <h3>Viewing all Applied Jobs</h3>
