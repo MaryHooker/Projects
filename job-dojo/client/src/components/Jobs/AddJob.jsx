c029e37f91d4c609ff2e3d077f44aff9a90810a0
MaryHooker <mc020791@yahoo.com>
2020-11-05 10:19:30 -0600

    handling changes to input fields

--- job-dojo/client/src/components/Jobs/AddJob.jsx
+++ job-dojo/client/src/components/Jobs/AddJob.jsx
@@ -1,17 +1,61 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 class AddJob extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "",
+      title: "",
+      description: "",
+      website: "",
     }
-    render() { 
-        return ( 
-            <div>
-                <h3>Add a job!</h3>
+  }
+
+  //sanity
+  componentDidMount() {
+    console.log('Adding new job!');
+  }
+
+  //function to update input fields in state
+  handleChanges = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+    //sanity
+    console.log(`Updating input fields ${JSON.stringify(this.state)}`)
+  }
+
+  render() {
+    return (
+      <div>
+                <form action="">
+                    <br/>
+                    <div>
+                        <label htmlFor="company">Company: </label>
+                        <br/>
+                        <input type="text" id="company" name="company" onChange={this.handleChanges} value={this.state.company}/>
+                    </div>
+                    <div>
+                        <label htmlFor="title">Title: </label>
+                        <br/>
+                        <input type="text" id="title" name="title" onChange={this.handleChanges} value={this.state.title}/>
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description: </label>
+                        <br/>
+                        <textarea name="description" cols="30" rows="10" id="description" name="description" onChange={this.handleChanges} value={this.state.description}/>
+                    </div>
+                    <div>
+                        <label htmlFor="website">Website: </label>
+                        <br/>
+                        <input type="text" id="website" name="website" onChange={this.handleChanges} value={this.state.website}/>
+                    </div>
+                    <div>
+                        <button onClick={this.handleSubmission}>Submit</button>
+                    </div>
+                </form>
             </div>
-         );
-    }
-}
- 
+      );
+  }}
+
 export default AddJob;
