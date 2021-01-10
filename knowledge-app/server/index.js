0afcb23a7d6df7dbda33a30e7298174ef47cfcaa
MaryHooker <mc020791@yahoo.com>
2021-01-10 15:18:06 -0600

    general terminal knowledge model created

--- knowledge-app/server/index.js
+++ knowledge-app/server/index.js
@@ -5,7 +5,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//import routes
+
+//connect to mongoDB
+
 //listen to server
 app.listen(1991, () => {
     console.log('listening on port 1991')
-})
+})  
