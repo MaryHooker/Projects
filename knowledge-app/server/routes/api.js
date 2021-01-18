d420f70c316b1026f6bffc334d54293f78bb22f7
MaryHooker <mc020791@yahoo.com>
2021-01-18 12:10:52 -0600

    skeleton crud endpoints for languages created and tested

--- knowledge-app/server/routes/api.js
+++ knowledge-app/server/routes/api.js
@@ -14,17 +14,28 @@ const router = express.Router();
 
 //create a language
 router.post('/language', (req,res) => {
-    // console.log(`language created!`);
     res.send(`language created!`)
 })
 
 //read a language
+router.get('/language/one', (req,res) => {
+    res.send(`one language read!`)
+})
 
 //update a language
+router.put('/language', (req,res) => {
+    res.send(`language updated!`)
+})
 
 //delete a language
+router.delete('/language', (req,res) => {
+    res.send(`language deleted!`)
+})
 
 //read all languages
+router.get('/languages', (req,res) => {
+    res.send(`reading all languages!`)
+})
 
 
 
